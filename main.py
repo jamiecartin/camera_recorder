f13d8892fe0b43498af1fcf57388712da20a34a6
jaseyaws <jcartin@ykw.systems>
2023-02-28 15:34:14 -0500

    set up key press to start and stop recording

--- main.py
+++ main.py
@@ -7,4 +7,26 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 writer = cv2.VideoWriter("recording.mp4", fourcc, 30.0, (1280, 720))
+recording = False
 
+while True:
+    ret, frame = cap.read()
+    
+    if ret:
+        cv2.imshow("video", frame)
+        if recording:
+            writer.write(frame)
+    
+    
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    elif key == ord('r'):
+        recording = not recording
+        print(f"Recording: {recording}")
+        
+        
+cap.release()
+writer.release()
+
+cv2.destroyAllWindows()
