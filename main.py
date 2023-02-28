183ef52fb614cbd7d3774d121e234fb3adbbfe60
jaseyaws <jcartin@ykw.systems>
2023-02-28 15:26:54 -0500

    codec

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+writer = cv2.VideoWriter("recording.mp4", fourcc, 30.0, (1280, 720))
+
